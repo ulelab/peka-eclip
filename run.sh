9f6385be7a4a4f15e5cfd6609f2736dc63a1ceb7
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-09-02 00:25:04 +0100

    Added run script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+ml purge
+ml Nextflow/21.04.0
+ml Singularity/3.6.4
+ml Graphviz/2.38.0-foss-2016b
+
+nextflow pull amchakra/peka-eclip -r main
+
+nextflow run amchakra/peka-eclip -r main --input './data'
