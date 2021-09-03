a89d72c50fa738a8dc3f7b579b2229cb7e12485e
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-09-03 12:14:02 +0100

    Updated clipseq run

--- preprocessing/samplesheet.R
+++ preprocessing/samplesheet.R
@@ -8,3 +8,16 @@ dt <- data.table(fastq = list.files(pattern = ".fastq.gz$", path = fastq.path, f
 dt[, sample := tstrsplit(basename(fastq), "\\.")[1]]
 setcolorder(dt, c("sample", "fastq"))
 fwrite(dt, "samplesheet.csv")
+
+missing <- c("HepG2-RBM5_mock-1", 
+"HepG2-TROVE2_mock-1", 
+"K562-GEMIN5_mock-1", 
+"K562-HLTF_mock-1", 
+"K562-RPS3_mock-1", 
+"K562-RPS3-1", 
+"K562-SSB-1", 
+"K562-SSB-2", 
+"K562-TROVE2-2")
+
+sel.dt <- dt[!sample %in% missing]
+fwrite(sel.dt , "sel.samplesheet.csv")
--- run_clipseq.sh
+++ run_clipseq.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 ml purge
 ml Nextflow/21.04.0
@@ -9,10 +9,10 @@ export NXF_SINGULARITY_CACHEDIR=/camp/lab/luscomben/home/shared/singularity
 REFDIR=/camp/lab/luscomben/home/users/chakraa2/projects/peka/eclip/ref
 
 ## UPDATE PIPELINE
-nextflow pull amchakra/clipseq -r feat-dedup-bug
+nextflow pull amchakra/clipseq -r dev
 
 ## RUN PIPELINE
-nextflow run amchakra/clipseq -r feat-dedup-bug \
+nextflow run amchakra/clipseq -r dev \
 -profile crick \
 --input samplesheet.csv \
 --outdir results_clipseq \
