d84f86f03576d9ee5923d43b61a99e59d79d15a0
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-09-28 22:53:07 +0100

    Updated download scripts

---
+++ preprocessing/download_multi.R
@@ -0,0 +1,37 @@
+# Script to download and rename ENCODE fastq on CAMP for multi-fastq
+# A. M. Chakrabarti
+# Last updated 26th October 2018, 1st September 2021
+
+library(data.table)
+
+metadata.dt <- fread("metadata.tsv")
+metadata.dt[, output := paste0(`Biosample term name`, "-",
+                               gsub(" eCLIP mock input", "_mock", sapply(strsplit(`Experiment target`, "\\-"), "[[", 1)), "-",
+                               `Biological replicate(s)`, ".",
+                               `Paired end`, ".fastq.gz")]
+
+# NB K562 mock NONO has an extra space in the Experiment target
+metadata.dt[output == "K562-NONO _mock-1.1.fastq.gz", output := "K562-NONO_mock-1.1.fastq.gz"]
+metadata.dt[output == "K562-NONO _mock-1.2.fastq.gz", output := "K562-NONO_mock-1.2.fastq.gz"]
+
+
+setwd("data/many")
+# NB 3 have more than 2 fastq to sample, so get overwritten
+metadata.dt[, fastq_count := .N, by = .(`Biosample term name`, `Experiment target`, `Biological replicate(s)`)]
+many.dt <- metadata.dt[fastq_count > 2]
+lapply(1:nrow(many.dt), function(i) download.file(many.dt$`File download URL`[i], destfile = basename(many.dt$`File download URL`[i]), method = "wget"))
+
+
+many.list <- split(many.dt[, .(`File download URL`, output)], many.dt$output)
+
+lapply(seq_along(many.list), function(i) {
+  
+  cmd <- paste0("zcat ", paste0(basename(many.list[[i]]$`File download URL`), collapse = " "), " | pigz > ", paste0("temp_", names(many.list)[i]))
+  print(cmd)
+  system(cmd)
+  
+})
+
+system("repair.sh -Xmx20g ain=t in=temp_HepG2-EXOSC5-2.1.fastq.gz in2=temp_HepG2-EXOSC5-2.2.fastq.gz out=HepG2-EXOSC5-2.1.fastq.gz out2=HepG2-EXOSC5-2.2.fastq.gz")
+system("repair.sh -Xmx20g ain=t in=temp_HepG2-FTO-1.1.fastq.gz in2=temp_HepG2-FTO-1.2.fastq.gz out=HepG2-FTO-1.1.fastq.gz out2=HepG2-FTO-1.2.fastq.gz")
+system("repair.sh -Xmx20g ain=t in=temp_K562-APOBEC3C-1.1.fastq.gz in2=temp_K562-APOBEC3C-1.2.fastq.gz out=K562-APOBEC3C-1.1.fastq.gz out2=K562-APOBEC3C-1.2.fastq.gz")
