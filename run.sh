1d9741ffe86bd34b289c07f427316af83a2d1eac
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2022-01-06 16:46:07 +0000

    Update run.sh after repo move

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ ml Nextflow/21.04.0
 ml Singularity/3.6.4
 ml Graphviz/2.38.0-foss-2016b
 
-nextflow pull amchakra/peka-eclip -r main
+nextflow pull ulelab/peka-eclip -r main
 
-nextflow run amchakra/peka-eclip -r main --input './data'
+nextflow run ulelab/peka-eclip -r main --input './data'
